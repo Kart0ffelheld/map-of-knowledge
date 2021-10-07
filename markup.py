6fb2e1bd4032dba5820302745ac9c634a2c9e91b
despudelskern <85169818+despudelskern@users.noreply.github.com>
2021-10-07 14:08:56 +0200

    Update markup.py

--- markup.py
+++ markup.py
@@ -154,7 +154,7 @@ app.layout = html.Div([
     dbc.Container([
         html.H1("Map of Knowledge),
         html.H5("Have fun with the progam!"),
-	hmtl.Div(dbc.button("Info", id="info" outline=True, color="info", size="lg", className="mr-1")),
+	hmtl.Div(info),
         html.Hr(),
         dbc.Row([
                 dbc.Col(controls, md=4),
